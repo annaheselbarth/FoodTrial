082bcc92ef825d2feddc9c3e90d2f674c3ece76f
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-31 11:13:27 -0500

    food, trial, medicine classes added

---
+++ FoodTrial.Data/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTrial.Data
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public virtual  Trial Trial { get; set; }
+        public string Name { get; set; }
+        public string Dose { get; set; }
+        public int Frequency { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ FoodTrial.Data/Trial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTrial.Data
+{
+    public class Trial
+    {
+        public int TrialId { get; set; }
+        public virtual Food Food { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateTime { get; set; }
+        public int NumberOfDays { get; set; }
+        public bool MedicalIntervention { get; set; }
+        //public Symptoms Symptoms { get; set; }
+        //public TypeOfReaction ReactionType { get; set; }
+        public bool IsSafe { get; set; }
+
+    }
+}
