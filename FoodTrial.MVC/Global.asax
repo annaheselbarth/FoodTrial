f8d2603a863a6e4909165777e4e9c72cb38edb4e
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-31 09:56:55 -0500

    Add project files.

---
+++ FoodTrial.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodTrial.MVC.MvcApplication" Language="C#" %>
---
+++ FoodTrial.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodTrial.MVC.Startup))]
+namespace FoodTrial.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
