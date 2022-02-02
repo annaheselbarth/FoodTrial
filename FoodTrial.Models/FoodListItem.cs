35469e5298effa38d1556c87a9029801aa63500c
Anna Heselbarth <annaheselbarth@gmail.com>
2022-02-02 12:32:18 -0500

    added listitem classes

---
+++ FoodTrial.Models/FoodListItem.cs
@@ -0,0 +1,18 @@
+﻿using FoodTrial.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTrial.Models
+{
+    public class FoodListItem
+    {
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+        public AllergenGroup AllergenGroup { get; set; }
+        public FoodGroup FoodGroup { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FoodTrial.Models/TrialListItem.cs
@@ -0,0 +1,22 @@
+﻿using FoodTrial.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTrial.Models
+{
+    public class TrialListItem
+    {
+        public int TrialId { get; set; }
+        public int FoodId { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateTime { get; set; }
+        public int NumberOfDays { get; set; }
+        //public bool MedicalIntervention { get; set; }
+        public Symptoms Symptoms { get; set; }
+        public TypeOfReaction ReactionType { get; set; }
+        //public bool IsSafe { get; set; }
+    }
+}
